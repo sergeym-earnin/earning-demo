fe2283334cd99f09687831825e3a5e08dd045196
mikhails-earnin <mikhail.sorokovsky@earnin.com>
2017-11-27 19:22:47 -0800

    add keys to gitignore and create it when needed

--- kube-infrastructure/scripts/generate_certificate.sh
+++ kube-infrastructure/scripts/generate_certificate.sh
@@ -2,7 +2,10 @@
 set -e
 CERTIFICATE_NAME=$(scripts/get_cluster_name.py)-certificate
 
+mkdir -p keys
+
 ssh-keygen -t rsa -b 4096 -N '' -C "$CERTIFICATE_NAME" -f "keys/$CERTIFICATE_NAME"
+
 mkdir -p ~/.ssh
 mv keys/$CERTIFICATE_NAME ~/.ssh/$CERTIFICATE_NAME
 chmod 600 ~/.ssh/$CERTIFICATE_NAME
